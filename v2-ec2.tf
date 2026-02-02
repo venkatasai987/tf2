2005e7abafe1fdcc8a5f3d66850973bf6371b65d
venkatasai987 <venkatsai2036@gmail.com>
2026-02-02 15:35:17 +0530

    2

---
+++ v2-ec2.tf
@@ -0,0 +1,33 @@
+ provider "aws" {
+region  = "ap-south-1"
+}
+ resource "aws_instance" "web" {
+  ami           = "ami-019715e0d74f695be"
+  instance_type = "t2.micro"
+  key_name      = "n"
+ }
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
+}
